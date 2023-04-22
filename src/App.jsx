a03d47129f67bd8d2527df1827a241417fd13446
Łucja Kunikowska <lucja.kun2@gmail.com>
2023-04-22 13:49:14 +0200

    Add notes file, mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes";
+
 
 function App() {
   return (
-    <>
-      <Header />
-      <Note />
+      <div>
+        <Header />
+        {notes.map((input) => 
+        <Note 
+          key={input.key}
+          title={input.title}
+          content = {input.content}
+        />
+      )}
       <Footer />
-    </>
+      </div>
   );
 }
 
